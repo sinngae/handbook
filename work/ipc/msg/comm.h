69b74bd10a2a3a868fb6de3dc011e4e286c87d37
root <root@localhost.localdomain>
2018-09-10 18:48:39 -0400

    commit  daily

---
+++ work/ipc/msg/comm.h
@@ -0,0 +1,26 @@
+﻿#ifndef _COMM_H_
+#define _COMM_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/msg.h>
+
+struct msgbuf
+{
+    long mtype;
+    char mtext[1024];
+};
+
+#define SERVER_TYPE 1
+#define CLIENT_TYPE 2
+
+int createMsgQueue();
+int getMsgQueue();
+int destoryMsgQueue(int msg_id);
+int sendMsgQueue(int msg_id, int who, char* msg);
+int recvMsgQueue(int msg_id, int recvType, char out[]);
+
+#endif
