4cc26cfb18258e2a48ca78a257d83e973ec982fe
root <root@localhost.localdomain>
2018-09-28 10:21:01 +0800

    add autotools

---
+++ work/autobuild/autotools/auto_test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("auto make test\n");
+	return 0;
+}
---
+++ work/autobuild/autotools/config.h
@@ -0,0 +1,26 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "auto_test"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "zqren@foxmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "auto_test"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "auto_test 1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "auto_test"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0"
+
+/* Version number of package */
+#define VERSION "1.0"
