ba72315f5267c00699fe4103d325a97073a81acb
renziqiang <renziqiang@xunlei.com>
2019-08-06 15:48:45 +0800

    add mlock

--- work/defect/sigbus/example1.c
+++ work/defect/sigbus/example1.c
@@ -11,32 +11,32 @@
 #include <signal.h>
 #include <sys/mman.h>
 #include <unistd.h>
- 
+
 void handle_sigbus(int sig)
 {
     printf("SIGBUS!\n");
     _exit(0);
 }
- 
+
 int main()
 {
     int i;
     char *p, tmp;
     struct stat st;
     int fd = -1;
- 
+
     fd = open("aaa.txt", O_RDWR);
     stat("aaa.txt", &st);
     p = (char*)mmap(NULL, st.st_size, PROT_READ|PROT_WRITE,MAP_SHARED, fd, 0);
     signal(SIGBUS, handle_sigbus);
- 
+
     getchar();
- 
+
     for (i = 0; i < st.st_size; i++) {
-            tmp = p[i];
-            printf("%x\n", tmp);
-        }
- 
+        tmp = p[i];
+        printf("%x\n", tmp);
+    }
+
     printf("ok\n");
 
     return 0;
