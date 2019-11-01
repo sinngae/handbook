ffb3835a5791e2322cbc111cc79f42ad72be7326
ArenPeace <zqren@foxmail.com>
2019-11-01 20:51:32 +0800

    add description;..

--- tools/clear_none_image.sh
+++ tools/clear_none_image.sh
@@ -1,5 +1,6 @@
 #########################################
 # File Name: clear_none_image.sh
+#		clear docker images and containers which are marked none
 # Author: Sinngae
 # mail: zqren@foxmail.com
 # Created Time: Wed 10 Apr 2019 09:20:38 PM CST
