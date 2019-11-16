15009a0c094f39cf61979b02fa6c692f5920e3ee
ArenPeace <zqren@foxmail.com>
2019-11-16 10:34:59 +0800

    read gopl daily

--- tech-particle/otherlang/go/gopl-examples/maptest.go
+++ tech-particle/otherlang/go/gopl-examples/maptest.go
@@ -5,15 +5,29 @@ import (
 )
 
 func main() {
-    mmap := map[string]string{"key":"value", "a":"b", "1":"2", "df":"adf"}
-    fmt.Println(mmap)
+    /*mmap := map[string]string{"key":"value", "a":"b", "1":"2", "df":"adf"}
+    fmt.Printf("map: %v\n", mmap)
 
-    key, ok := mmap["a"];
-    if ok {
-        fmt.Println(mmap["a"])
-        fmt.Println(key)
+    if value, ok := mmap["a"]; ok {
+        fmt.Printf("value: %v\n", value)
+        value = "test"
+        fmt.Printf("value: %v\n", mmap["a"])
+        mmap["a"] = "test"
+        fmt.Printf("value: %v\n", mmap["a"])
+    }*/
+
+    type MyType struct {
+        A   int32
+        B   int32
     }
-    if key, ok = mmap["1"]; ok {
-        fmt.Println(mmap["1"])
+    //var myTypeMap map[int32]MyType
+    myTypeMap := make(map[int32]*MyType, 2)
+    fmt.Printf("myTypeMap: %v\n", myTypeMap)
+    //fmt.Printf("myTypeMap[1].A: %v\n", myTypeMap[1].A)
+    if _, ok := myTypeMap[1] ; !ok {
+        myTypeMap[1] = new(MyType)
     }
+    myTypeMap[1].A += 1
+    //myTypeMap[2].B = 2
+    fmt.Printf("myTypeMap: %v\n", myTypeMap)
 }
