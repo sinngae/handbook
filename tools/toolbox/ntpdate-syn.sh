0bde6560de6e8a8276d25c17540e12cc76c8e54a
ArenPeace <zqren@foxmail.com>
2020-03-16 09:39:55 +0800

    enrich ntpdate

--- tools/toolbox/ntpdate-syn.sh
+++ tools/toolbox/ntpdate-syn.sh
@@ -2,17 +2,3 @@
 # 手动同步
 #   yum install -y ntp
 ntpdate time.nist.gov
-
-# 时间自动同步
-#   crontab -e
-#   添加：*/10 * * * * ntpdate time.nist.gov    # 每隔十分钟同步一次
-#   添加：30 5 * * * ntpdate time.nist.gov      # 每天5点30分同步一次
-
-# 几个时间服务器
-#   time.nist.gov
-#
-
-# 相关命令
-#   hwclock --show      # CMOS时间，硬件时间
-#   date -R             # 系统时间
-#   clock --systohc     # 硬件时间同步系统时间
